217875512c32d232e1f7a30fb3e4a108d24601d5
Artur <artur_r10@mail.ru>
2024-01-27 18:56:38 +0300

    Initial Commit

---
+++ TrafficLight2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight2
+//
+//  Created by Карина Короткая on 27.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
